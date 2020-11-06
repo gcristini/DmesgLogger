17290bb4ad37fe22353136b728a0d29190002ae7
gcristini <gabriele.cristini@datalogic,com>
2020-11-06 17:17:34 +0100

    Added SX5_Manager.py, many additions.... TODO: test and exit mechanism

---
+++ Sources/Libraries/SX5_Manager.py
@@ -0,0 +1,151 @@
+import subprocess
+from adb_shell.adb_device import AdbDeviceTcp
+from adb_shell import exceptions
+from Libraries.Timer import Timer
+import sys
+import json
+
+class ADB_Error(Exception):
+    # TODO
+    pass
+
+class SX5_Manager(object):
+    """ """
+    # ************************************************* #
+    # **************** Private Methods **************** #
+    # ************************************************* #
+    def __init__(self):
+        """ Constructor"""
+        
+        self._sx5_config_dict: dict
+        self._sx5_device: AdbDeviceTcp
+
+        pass
+
+    # ---------------------------------------------------------------- #
+    # ----------------------- Private Methods ------------------------ #
+    # ---------------------------------------------------------------- #
+    def _parse_config_file(self):
+        """"""
+        with open("Config.json") as json_file:
+            self._sx5_config_dict = json.load(json_file)
+
+        pass
+
+    def _adb_init(self):
+        """"""
+        count = 0
+        res = -1
+        num_of_try = 60
+
+        # Start Timer
+        timer = Timer()
+        timer.start()
+
+        while (count < num_of_try):
+            if timer.elapsed_time_s(2) >= 1:
+                print("- Waiting for SX5 device " + "."*count, end='\r')
+                res = subprocess.run("adb devices", text=True, capture_output=True).stdout.find(self._sx5_config_dict['device']['name'])
+                if (res != -1):
+                    timer.stop()
+                    break
+                else:
+                    timer.reset()
+
+                count += 1
+        if res == -1:
+            sys.stdout.write("\033[K")
+            print("No Device Found")
+            raise ADB_Error
+        else:
+            sys.stdout.write("\033[K")
+            self._sx5_device = AdbDeviceTcp(host=self._sx5_config_dict['device']['ip'],
+                                            port=int(self._sx5_config_dict['device']['port']),
+                                            default_transport_timeout_s=9.)
+        pass
+
+    def _adb_tcp_connect(self):
+        """"""
+        try:
+            self._sx5_device.connect(auth_timeout_s=0.1)
+            print("- SX5 Connected")
+        except:
+            print("Timeout expired: check if device is on and if the IP is correct")
+            raise
+        pass
+
+    def _adb_tcp_disconnect(self):
+        """"""
+        self._sx5_device.close()
+        pass
+
+    # ---------------------------------------------------------------- #
+    # ------------------------ Public Methods ------------------------ #
+    # ---------------------------------------------------------------- #
+
+    def init(self):
+        """ Initialize class and connect SX5 to ADB over TCP-IP protocol """
+        # Parse config file
+        self._parse_config_file()
+
+        # Initialize adb connection
+        try:
+            self._adb_init()
+        except ADB_Error:
+            sys.exit()
+        try:
+            self._adb_tcp_connect()
+        except:
+            subprocess.run("adb disconnect")
+            subprocess.run("adb tcpip {port}".format(port=int(self._sx5_config_dict['device']['port'])))
+            #self._adb_init()
+            self._adb_tcp_connect()
+        pass
+
+    def _read_shell(self, cmd: str):
+        """"""
+        max_attempt = 10
+        attempt_count = 0
+        stdout = ""
+
+        while attempt_count < max_attempt:
+            try:
+                stdout = self._sx5_device.shell(cmd)
+            except:
+                # Try to establish a new connection if no response...
+                try:
+                    self._sx5_device = AdbDeviceTcp(host=self._sx5_config_dict['device']['ip'],
+                                                    port=int(self._sx5_config_dict['device']['port']),
+                                                    default_transport_timeout_s=9.)
+                except:
+                    pass
+                try:
+                    self._sx5_device.connect(auth_timeout_s=0.2)
+                except:
+                    pass
+
+                attempt_count += 1
+            else:
+                break
+
+        if attempt_count >= max_attempt:
+            raise exceptions.TcpTimeoutException
+        pass
+
+        return stdout
+
+    def read_dmesg(self, value: str):
+        """ """
+        # TODO fare check
+        #stdout = self._read_shell('adb shell "dmesg -c -T"')
+        stdout = self._read_shell('dmesg -c -T')
+
+        return stdout
+        
+
+
+
+
+
+
+
--- Sources/Main.py
+++ Sources/Main.py
@@ -8,11 +8,14 @@ import json
 
 import colorama as cm
 
+
 #from Libraries.Enums import Enums as en
 #from Libraries.ParseXml import XmlDictConfig
 #from xml.etree import ElementTree
 
 from Libraries.Timer import Timer
+from Libraries.SX5_Manager import SX5_Manager
+from adb_shell import exceptions
 
 class MainStateEnum:
     MAIN_STATE_INIT = "init"
@@ -38,12 +41,16 @@ class Main(object):
             "log_files": {}
         }
 
+        self._sx5: SX5_Manager
+
+        self._global_timer: Timer
+
         # State Machine
         self._main_state_fun_dict = {
             MainStateEnum.MAIN_STATE_INIT: self._init_state_manager,
             MainStateEnum.MAIN_STATE_CREATE_FOLDER: self._create_folder_state_manager,
             MainStateEnum.MAIN_STATE_ADB_CONNECT: self._adb_connect_state_manager,
-            MainStateEnum.MAIN_STATE_DMESG: self._main_state_dmesg_manager,
+            MainStateEnum.MAIN_STATE_DMESG: self._dmesg_state_manager,
             MainStateEnum.MAIN_STATE_STOP: self._stop_state_manager,
             MainStateEnum.MAIN_STATE_EXIT: self._exit_state_manager
         }
@@ -90,8 +97,13 @@ class Main(object):
         print(cm.Fore.MAGENTA + " ------------------------------------------------------------ ")
         print(cm.Fore.MAGENTA + " ----------------- WELCOME TO DMESG LOGGER! ----------------- ")
         print(cm.Fore.MAGENTA + " ------------------------------------------------------------ ")
+        
+        self._sx5.init()
 
-        self._go_to_next_state (MainStateEnum.MAIN_STATE_CREATE_FOLDER)
+        self._global_timer.start()
+
+        # Go to create folder state
+        self._go_to_next_state(MainStateEnum.MAIN_STATE_CREATE_FOLDER)
         return
     
     def _create_folder_state_manager(self):
@@ -108,17 +120,23 @@ class Main(object):
         os.mkdir(self._log_file_dict["storage"])
         
         # Create log files
+        self._log_file_dict["log_files"].update({"raw": f'{self._log_file_dict["storage"]}/RawLog.txt'})
+        os.mkdir(self._log_file_dict["log_files"]["raw"])
+
         for key,value in self._main_config_dict["log"].items():
             if value == True:
-                self._log_file_dict["log_files"].update({key: f'{ self._log_file_dict["storage"]}/{key}.txt'})
+                self._log_file_dict["log_files"].update({key: f'{self._log_file_dict["storage"]}/{key}.txt'})
                 os.mkdir(self._log_file_dict["log_files"][key])
 
-
-        self._go_to_next_state(MainStateEnum.MAIN_STATE_ADB_CONNECT)
+        # Go to ADB connect state
+        #self._go_to_next_state(MainStateEnum.MAIN_STATE_ADB_CONNECT)
+        
+        # Go to dmesg state manager
+        self._go_to_next_state(MainStateEnum.MAIN_STATE_DMESG)
         return
-
-    def _adb_connect_state_manager(self):
-        """"""
+    
+    def _adb_connect_state_manager(self): # TODO ELIMINARE !?
+        """"""        
         count = 0
         res = -1
         num_of_try = 60
@@ -155,12 +173,34 @@ class Main(object):
         
         return
 
-    def _main_state_dmesg_manager(self):
+    def _dmesg_state_manager(self):
         """"""
+        dmesg = ""
 
+        try:            
+            dmesg = self._sx5.read_dmesg()
+        except exceptions.TcpTimeoutException: 
+            # Go to stop state
+            self._go_to_next_state(MainStateEnum.MAIN_STATE_STOP)
+        else:
+            # Update raw log file
+            raw_log = open(file=self._log_file_dict['log_files']['raw'], mode='a', newline='')
+            raw_log.write(dmesg)
+            raw_log.close()
 
+            # Update single log files            
+            for keyword in self._log_file_dict['log_files'].keys():
+                # Open log file that correspond to keyword
+                log_file = open(file=self._log_file_dict['log_files'][keyword], mode='a', newline='')
 
-        self._go_to_next_state(MainStateEnum.MAIN_STATE_STOP)
+                for line in raw_log.split('\n'):
+                    if line.find(keyword):
+                        # If there is the keyword, update the log with the corresponding line
+                        log_file.write(line)
+                        
+                # Close the log file
+                log_file.close()
+                    
         return
 
     def _stop_state_manager(self):
@@ -201,18 +241,19 @@ class Main(object):
 
     def run(self):
         """ Main Application """
-
+        
         # Init
         self._init_state_manager()
-
+        
         while not (self._main_state == MainStateEnum.MAIN_STATE_STOP and
                    self._last_main_state == MainStateEnum.MAIN_STATE_STOP):
+            
+            if self._global_timer.elapsed_time_s() >= 1:
+                # Store the last state machine state
+                #self._store_last_state()
 
-            # Store the last state machine state
-            #self._store_last_state()
-
-            # Run state machine at current state
-            self._main_state_machine_manager()
+                # Run state machine at current state
+                self._main_state_machine_manager()
       
 if __name__ == "__main__":    
     test = Main()
